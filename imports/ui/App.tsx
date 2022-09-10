ca16770490b7225676c2a22f7cbbb313fcf9bffc
v-l-a-d-z <>
2022-09-10 23:33:22 +0200

    T2 - Add user badge to app header

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -5,13 +5,19 @@ import { useTracker } from 'meteor/react-meteor-data'
 
 export const App = () => {
   const user = useTracker(() => Meteor.user())
+  const logout = () => Meteor.logout()
 
   return (
     <div className="app">
       <header className="app-bar">
         <h1>Appointments</h1>
+        {user && (
+          <div className="user-badge" onClick={logout}>
+            {user.username}
+            <button onClick={logout}>→</button>
+          </div>
+        )}
       </header>
-
       <div className="main">
         {user ? <h1>Welcome {user.username}</h1> : <LoginScreen />}
       </div>
