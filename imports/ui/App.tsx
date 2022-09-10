6f5979c5ee6629dc6c844f2837420c7bb53eeaaf
v-l-a-d-z <>
2022-09-10 13:54:40 +0200

    T1 - Clean up sample code

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import { Hello } from './Hello';
-import { Info } from './Info';
+import React from "react"
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
   </div>
-);
+)
