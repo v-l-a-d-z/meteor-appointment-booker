5b0b7b4057deabd86bbc9cf4c1612fce57aa04d9
v-l-a-d-z <>
2022-09-10 23:31:07 +0200

    T2 - Display Login screen when not logged in

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,7 +1,20 @@
-import React from "react"
+import React from 'react'
+import { Meteor } from 'meteor/meteor'
+import LoginScreen from './screens/LoginScreen'
+import { useTracker } from 'meteor/react-meteor-data'
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-  </div>
-)
+export const App = () => {
+  const user = useTracker(() => Meteor.user())
+
+  return (
+    <div className="app">
+      <header className="app-bar">
+        <h1>Appointments</h1>
+      </header>
+
+      <div className="main">
+        {user ? <h1>Welcome {user.username}</h1> : <LoginScreen />}
+      </div>
+    </div>
+  )
+}
