21c86732632ff021cadcc2c1d0132260933f1fdb
v-l-a-d-z <>
2022-09-11 14:49:57 +0200

    T3 - Remove redundant typechecking

--- imports/api/appointmentsMethods.ts
+++ imports/api/appointmentsMethods.ts
@@ -1,13 +1,8 @@
 import { Meteor } from 'meteor/meteor'
-import { check } from 'meteor/check'
 import { Appointments } from '../db/Appointments'
 
 Meteor.methods({
   'appointments.insert'(firstName: string, lastName: string, date: Date) {
-    check(firstName, String)
-    check(lastName, String)
-    check(date, Date)
-
     if (!this.userId) {
       throw new Meteor.Error('Not authorized to insert appointment.')
     }
