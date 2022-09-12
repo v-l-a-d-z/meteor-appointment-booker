cd78510daf2380594c665fb788a7547c16e38232
v-l-a-d-z <>
2022-09-12 13:11:37 +0200

    T4 - Seed one fixed appointment for test user

--- server/main.ts
+++ server/main.ts
@@ -2,12 +2,29 @@ import { Meteor } from 'meteor/meteor'
 import { Accounts } from 'meteor/accounts-base'
 import '/imports/api/appointmentsMethods'
 import '/imports/api/appointmentsPublications'
+import { Appointments } from '/imports/db/Appointments'
 
 const SEED_USERNAME1 = 'testuser1'
 const SEED_PASSWORD1 = 'pass'
 
 const SEED_USERNAME2 = 'testuser2'
 const SEED_PASSWORD2 = 'word'
+
+const seedAppointment = (username: string) => {
+  const user = Accounts.findUserByUsername(username)
+  if (!user) {
+    return
+  }
+
+  if (Appointments.find({}).count() == 0) {
+    Appointments.insert({
+      firstName: 'John',
+      lastName: 'Doe',
+      date: new Date(),
+      userId: user._id,
+    })
+  }
+}
 
 Meteor.startup(() => {
   if (!Accounts.findUserByUsername(SEED_USERNAME1)) {
@@ -23,4 +40,6 @@ Meteor.startup(() => {
       password: SEED_PASSWORD2,
     })
   }
+
+  seedAppointment(SEED_USERNAME1)
 })
